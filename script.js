6e398b648b3586b028c4599c9f28d27aff8326a2
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-11-09 16:17:28 +0200

    check if the email is valid and entered

--- script.js
+++ script.js
@@ -8,10 +8,18 @@ const password = document.getElementById('password');
 const form = document.getElementById('form');
 
 const name_error = document.getElementById('name_error');
-
+const email_error = document.getElementById('email_error');
 form.addEventListener('submit',(e)=>{
+
+  let emailCkeck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
+
   if(name.value === '' || name.value==null){
     e.preventDefault();
     name_error.innerHTML = "Name is required";
+  }
+
+  if(!email.value.match(emailCkeck)){
+    e.preventDefault();
+    email_error.innerHTML = "Valid Email is required";
   }
 })
