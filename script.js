07493e93ea9e516d17310773f85f7d8fd4e1c4e2
ishimwefrank123 <140093486+ishimwefrank123@users.noreply.github.com>
2024-11-09 16:31:11 +0200

    password validation

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const form = document.getElementById('form');
 
 const name_error = document.getElementById('name_error');
 const email_error = document.getElementById('email_error');
+const password_error = document.getElementById('password_error');
 form.addEventListener('submit',(e)=>{
 
   let emailCkeck = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/; 
@@ -21,5 +22,20 @@ form.addEventListener('submit',(e)=>{
   if(!email.value.match(emailCkeck)){
     e.preventDefault();
     email_error.innerHTML = "Valid Email is required";
+  }
+
+  if(password.value.length <= 5){
+    e.preventDefault();
+    password_error.innerHTML = "Password must be more than 6 characters";
+  }
+
+  if(password.value.length >= 20){
+    e.preventDefault();
+    password_error.innerHTML = "Password must not be more than 20 characters";
+  }
+
+  if(password.value ==='password'){
+    e.preventDefault();
+    password_error.innerHTML = "Password must not be password";
   }
 })
